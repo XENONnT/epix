6ccaaaeb510714f158022d31e0f60724363260d4
Diego Ramírez García <diego.ramirez@physik.uni-freiburg.de>
2021-01-12 20:18:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
             'nbsphinx',
             'recommonmark',
             'graphviz']},
-    scripts=['bin/run_epix],
+    scripts=['bin/run_epix'],
     packages=setuptools.find_packages(),
     classifiers=[
         'Natural Language :: English',
