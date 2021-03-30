f983998ea0ec740452ae867a63eae1eebf0e21bb
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2021-03-30 09:50:43 +0200

    E-Field format change (#23)
    * switch from csv to json.gz
    * Add option to use .csv files
    * bring name in line with other parts of epix
    * Update FieldHandler docstring
    * Back to cm for all efield formats
    * typos
    
    Co-authored-by: Diego Ramírez García <diego.ramirez@physik.uni-freiburg.de>

--- epix/__init__.py
+++ epix/__init__.py
@@ -6,7 +6,7 @@ from .clustering import *
 from .quanta_generation import *
 from .detector_volumes import *
 from .clustering import *
-from .ElectricFieldHandler import *
+from .electric_field_handler import *
 from .event_separation import *
 from .detectors import *
 from .run_epix import *
