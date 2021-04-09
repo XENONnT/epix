6a866747296ed6995f15e3281ff38311be14ddb3
Diego Ramírez García <diego.ramirez@physik.uni-freiburg.de>
2021-04-09 13:03:49 +0200

    Remove duplicated import

--- epix/__init__.py
+++ epix/__init__.py
@@ -5,7 +5,6 @@ from .io import *
 from .clustering import *
 from .quanta_generation import *
 from .detector_volumes import *
-from .clustering import *
 from .electric_field_handler import *
 from .event_separation import *
 from .detectors import *
