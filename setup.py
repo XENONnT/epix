3df7f2c302afe067aeb8adfe30f381bf06c4cf79
ramirezdiego <diego.ramirez@physik.uni-freiburg.de>
2022-05-20 06:46:40 -0500

    Bump version: 0.2.2 → 0.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.md') as file:
 
 setuptools.setup(
     name='epix',
-    version='0.2.2',
+    version='0.3.0',
     description='Electron and Photon Instructions generator for XENON',
     author='epix contributors, the XENON collaboration',
     url='https://github.com/XENONnT/epix',
