6b16073c7ca258d73b105e30a98af5d977fdd45b
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2023-03-27 14:23:26 +0200

    Add Input Plugin

--- epix/run_epix_refactor.py
+++ epix/run_epix_refactor.py
@@ -1,13 +1,100 @@
-## epix structure proposal 
+## epix structure proposal
+import strax
+import numpy as np
 
-class input_plugin(strax.plugin):
-    depends_on = None
+@strax.takes_config(
+    strax.Option('path', default=".", track=False, infer_type=False,
+                 help="Path to search for data"),
+    strax.Option('file_name', track=False, infer_type=False,
+                 help="File to open"),
+    strax.Option('debug', default=False, track=False, infer_type=False,
+                 help="Show debug informations"),
+    strax.Option('outer_cylinder', default=False, track=False, infer_type=False,
+                 help="Dont know"),
+    strax.Option('entry_start', default=0, track=False, infer_type=False,
+                 help="First event to be read"),
+    strax.Option('entry_stop', default=10, track=False, infer_type=False,
+                 help="How many entries from the ROOT file you want to process"),
+    strax.Option('cut_by_eventid', default=False, track=False, infer_type=False,
+                 help="If selected, the next two arguments act on the G4 event id, and not the entry number (default)"),
+    strax.Option('nr_only', default=False, track=False, infer_type=False,
+                 help="Add if you want to filter only nuclear recoil events (maximum ER energy deposit 10 keV)"), 
+)
+class input_plugin(strax.Plugin):
+    
+    __version__ = "0.0.0"
+    
+    depends_on = tuple()
     provides = "geant4_interactions"
-    dtype = "some_list_of_geant4_interactions"
+    
+    source_done = False
+    
+    dtype = [('x', np.float32),
+             ('y', np.float32),
+             ('z', np.float32),
+             ('t', np.float64),
+             ('ed', np.float32),
+             ('type', "<U10"),
+             ('trackid', np.int32),
+             ('parenttype', "<U10"),
+             ('parentid', np.int32),
+             ('creaproc', "<U10"),
+             ('edproc', "<U10"),
+             ('evtid', np.int32),
+             ('structure', np.int32),
+            ]
+    
+    dtype = dtype + strax.time_fields
 
-    def compute():
-        """Use the epix io functions here"""
-        pass
+    def setup(self):
+
+        self.epix_file_loader = epix.file_loader(self.path,
+                                                 self.file_name,
+                                                 self.debug,
+                                                 outer_cylinder=self.outer_cylinder,
+                                                 kwargs={'entry_start': self.entry_start,
+                                                         'entry_stop': self.entry_stop},
+                                                 cut_by_eventid=self.cut_by_eventid,
+                                                 #cut_nr_only=self.nr_only,
+                                                )
+    
+    def full_array_to_numpy(self, array):
+    
+        len_output = len(epix.awkward_to_flat_numpy(array["x"]))
+        array_structure = np.array(epix.ak_num(array["x"]))
+        array_structure = np.pad(array_structure, [0, len_output-len(array_structure)],constant_values = -1)
+
+        numpy_data = np.zeros(len_output, dtype=self.dtype)
+
+        for field in array.fields:
+            numpy_data[field] = epix.awkward_to_flat_numpy(array[field])
+        numpy_data["structure"] = array_structure
+        
+        return numpy_data
+    
+
+    def compute(self):
+
+        inter, n_simulated_events = self.epix_file_loader.load_file()
+        
+        inter_reshaped = self.full_array_to_numpy(inter)
+        
+        inter_reshaped["time"] = (inter_reshaped["evtid"]+1) *1e9
+        inter_reshaped["endtime"] = inter_reshaped["time"] +1e7
+        
+        return self.chunk(start=inter_reshaped['time'][0],
+                          end=inter_reshaped['endtime'][-1],
+                          data=inter_reshaped,
+                          data_type='geant4_interactions')
+
+    
+    def is_ready(self, chunk):
+        #For now, just one single large chunk. 
+        return True if chunk == 0 else False
+
+    def source_finished(self):
+        return True
+       
 
 
 class clustering(strax.plugin):
