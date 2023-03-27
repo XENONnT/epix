840771396603f3374090fa4f6cd460bc37cf3f1a
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2023-03-27 15:48:52 +0200

    Add Clustering Plugin

--- epix/run_epix_refactor.py
+++ epix/run_epix_refactor.py
@@ -94,17 +94,72 @@ class input_plugin(strax.Plugin):
 
     def source_finished(self):
         return True
-       
 
 
-class clustering(strax.plugin):
+@strax.takes_config(
+    strax.Option('micro_separation', default=0.005, track=False, infer_type=False,
+                 help="DBSCAN clustering distance (mm)"),
+    strax.Option('micro_separation_time', default = 10, track=False, infer_type=False,
+                 help="Clustering time (ns)"),
+    strax.Option('debug', default=False, track=False, infer_type=False,
+                 help="Show debug informations"),
+    strax.Option('tag_cluster_by', default=False, track=False, infer_type=False,
+                 help="decide if you tag the cluster (particle type, energy depositing process)\
+                       according to first interaction in it (time) or most energetic (energy)"),
+)
+class clustering(strax.Plugin):
+    
+    __version__ = "0.0.0"
+    
     depends_on = "geant4_interactions"
     provides = "clustered_interactions"
-    dtype = "some_list_of_clustered_interactions"
+    
+    dtype = [('x', np.float32),
+             ('y', np.float32),
+             ('z', np.float32),
+             ('t', np.float64),
+             ('ed', np.float64),
+             ('nestid', np.int64),
+             ('A', np.int64),
+             ('Z', np.int64),
+             ('structure', np.int64),
+            ]
+    
+    dtype = dtype + strax.time_fields
+    
+    def full_array_to_numpy(self, array):
+    
+        len_output = len(epix.awkward_to_flat_numpy(array["x"]))
+        array_structure = np.array(epix.ak_num(array["x"]))
+        
+        fake_event_time = np.repeat(np.arange(len(result["t"])), array_structure)
+        
+        array_structure = np.pad(array_structure, [0, len_output-len(array_structure)],constant_values = -1)
 
-    def compute():
-        """Use the epix clustering functions here"""
-        pass
+        numpy_data = np.zeros(len_output, dtype=self.dtype)
+
+        for field in array.fields:
+            numpy_data[field] = epix.awkward_to_flat_numpy(array[field])
+        numpy_data["structure"] = array_structure
+        numpy_data["time"] = (fake_event_time+1)*1e9
+        numpy_data["endtime"] = numpy_data["time"] +1e7
+        
+        return numpy_data
+
+    def compute(self, geant4_interactions):
+        
+        inter = ak.from_numpy(np.empty(1, dtype=geant4_interactions.dtype))
+        structure = geant4_interactions["structure"][geant4_interactions["structure"]>=0]
+        
+        for field in inter.fields:
+            inter[field] =  epix.reshape_awkward(geant4_interactions[field], structure)
+        
+        inter = epix.find_cluster(inter, self.micro_separation/10, self.micro_separation_time)
+        result = epix.cluster(inter, self.tag_cluster_by == 'energy')
+        
+        result = self.full_array_to_numpy(result)
+        
+        return result
 
 class electic_field(strax.plugin):
     depends_on = "clustered_interactions"
