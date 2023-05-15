06105c6d8e587b1cb7fd96d4bbe0456cae675f01
jarongrigat <jaron.grigat@googlemail.com>
2023-05-15 12:36:37 +0300

    s2 area fix + reconstructed positions

--- epix/simulator/fast_simulator.py
+++ epix/simulator/fast_simulator.py
@@ -1,9 +1,9 @@
+import pickle
 import time
 
 import strax
 import straxen
-from straxen import InterpolatingMap, get_resource
-from wfsim.load_resource import load_config
+import wfsim.load_resource
 import epix
 import numpy as np
 import pandas as pd
@@ -14,7 +14,6 @@ from .GenerateEvents import GenerateEvents
 from .GenerateNveto import NVetoUtils
 from .helpers import Helpers
 import warnings
-import pickle
 
 # 2023-02-19: configuration_files:
 #   'nv_pmt_qe':'nveto_pmt_qe.json',
@@ -30,8 +29,8 @@ def monitor_time(prev_time, task):
     return t
 
 
-class Simulator():
-    '''Simulator class for epix to go from  epix instructions to fully processed data'''
+class Simulator:
+    """Simulator class for epix to go from  epix instructions to fully processed data"""
 
     def __init__(self, instructions_epix, config, resource):
         self.config = config
@@ -47,8 +46,6 @@ class Simulator():
         )
         self.instructions_epix = instructions_epix
 
-        #print(f"\n\nSimulator : Current directory [ {os.getcwd()} ]")
-
     def cluster_events(self, ):
         """Events have more than 1 s1/s2. Here we throw away all of them except the largest 2
          Take the position to be that of the main s2
@@ -57,8 +54,8 @@ class Simulator():
         First do the macro clustering. Clustered instructions will be flagged with amp=-1,
         so we can safely throw those out"""
         start_time = time.time()
-        self.tree = pickle.load(open('/dali/lgrandi/jgrigat/s2_separation/tree_v2_unoptimised', 'rb+'))
-        Helpers.macro_cluster_events(self.tree, self.instructions_epix, self.config)
+        self.nn_weights = pickle.load(open('/home/jgrigat/epix/epix/simulator/nn_weights.p', 'rb+'))
+        Helpers.macro_cluster_events(self.nn_weights, self.instructions_epix, self.config)
         self.instructions_epix = self.instructions_epix[self.instructions_epix['amp'] != -1]
 
         event_numbers = np.unique(self.instructions_epix['event_number'])
@@ -77,11 +74,7 @@ class Simulator():
             if len(s1) < 1 or len(s2) < 1:
                 continue
 
-            # FIXME maybe we need a macro clustering for s1s as well. Need to check.
             i['s1_area'] = np.sum(inst_s1['amp'])
-            #i['s1_area'] = inst_s1[s1[-1]]['amp']
-            #if len(s1) > 1:
-            #    i['alt_s1_area'] = inst_s1[s1[-2]]['amp']
 
             i['s2_area'] = inst_s2[s2[-1]]['amp']
             i['e_dep'] = inst_s2[s2[-1]]['e_dep']
@@ -89,13 +82,13 @@ class Simulator():
             if len(s2) > 1:
                 i['alt_s2_area'] = inst_s2[s2[-2]]['amp']
                 i['alt_e_dep'] = inst_s2[s2[-2]]['e_dep']
-                i['alt_s2_x'] = inst_s2[s2[-2]]['x']
-                i['alt_s2_y'] = inst_s2[s2[-2]]['y']
-                i['alt_s2_z'] = inst_s2[s2[-2]]['z']
+                i['alt_s2_x_true'] = inst_s2[s2[-2]]['x']
+                i['alt_s2_y_true'] = inst_s2[s2[-2]]['y']
+                i['alt_s2_z_true'] = inst_s2[s2[-2]]['z']
 
-            i['x'] = inst_s2[s2[-1]]['x']
-            i['y'] = inst_s2[s2[-1]]['y']
-            i['z'] = inst_s2[s2[-1]]['z']
+            i['x_true'] = inst_s2[s2[-1]]['x']
+            i['y_true'] = inst_s2[s2[-1]]['y']
+            i['z_true'] = inst_s2[s2[-1]]['z']
 
             i['x_pri'] = inst_s2[s2[-1]]['x_pri']
             i['y_pri'] = inst_s2[s2[-1]]['y_pri']
@@ -113,22 +106,21 @@ class Simulator():
 
     def simulate(self, ):
         for func in self.simulation_functions:
-            starttime = time.time()
-            func(instructions=self.instructions,
+            start = time.time()
+            func(i=self.instructions,
                  config=self.config,
                  resource=self.resource)
-            monitor_time(starttime, func.__name__)
+            monitor_time(start, func.__name__)
 
     def run_simulator(self, ):
         self.cluster_events()
+        # TODO this is for debug purposes - not super nice. We should have a fastsim_truth data type
         if isinstance(self.config['epix_config'].get('save_epix', None), str):
             file_name = self.config['epix_config']['file_name'].split('/')[-1][:-4] + '_instruction_after_macro_clustering'
             epix_path = os.path.join(self.config['epix_config']['save_epix'] ,file_name)
             print('Saving epix instruction: ', epix_path)
             np.save(epix_path, self.instructions)
-
         self.simulate()
-
         return self.instructions
 
 
@@ -156,22 +148,50 @@ class StraxSimulator(strax.Plugin):
     data_kind = immutabledict(zip(provides, provides))
     dtype = dict(events_tpc=[('time', np.int64),
                              ('endtime', np.int64),
-                             ('s1_area', np.float),
-                             ('s2_area', np.float),
                              ('cs1', np.float),
                              ('cs2', np.float),
-                             ('alt_s1_area', np.float),
-                             ('alt_s2_area', np.float),
-                             ('alt_cs1', np.float),
                              ('alt_cs2', np.float),
-                             ('x', np.float),
-                             ('y', np.float),
-                             ('z', np.float),
+                             ('drift_time', np.float),
+                             ('s1_area', np.float),
+                             ('s2_area', np.float),
+                             ('alt_s2_area', np.float),
+                             ('alt_s2_interaction_drift_time', np.float),
+                             ('s2_x', np.float),
+                             ('s2_y', np.float),
                              ('alt_s2_x', np.float),
                              ('alt_s2_y', np.float),
+                             ('x', np.float),
+                             ('alt_s2_x_fdc', np.float),
+                             ('y', np.float),
+                             ('alt_s2_y_fdc', np.float),
+                             ('r', np.float),
+                             ('alt_s2_r_fdc', np.float),
+                             ('z', np.float),
+                             ('z_dv_corr', np.float),
                              ('alt_s2_z', np.float),
-                             ('drift_time', np.float),
-                             ('alt_s2_drift_time', np.float),
+                             ('alt_s2_z_dv_corr', np.float),
+                             ('r_naive', np.float),
+                             ('alt_s2_r_naive', np.float),
+                             ('z_naive', np.float),
+                             ('alt_s2_z_naive', np.float),
+                             ('r_field_distortion_correction', np.float),
+                             ('alt_s2_r_field_distortion_correction', np.float),
+                             ('z_field_distortion_correction', np.float32),
+                             ('alt_s2_z_field_distortion_correction', np.float32),
+                             ('alt_s2_theta', np.float32),
+                             ('theta', np.float32),
+
+                             # Truth values
+                             ('x_true', np.float),
+                             ('y_true', np.float),
+                             ('z_true', np.float),
+                             ('r_true', np.float),
+                             ('theta_true', np.float),
+                             ('alt_s2_x_true', np.float),
+                             ('alt_s2_y_true', np.float),
+                             ('alt_s2_z_true', np.float),
+                             ('alt_s2_r_true', np.float),
+                             ('alt_s2_theta_true', np.float),
                              ('e_dep', np.float),
                              ('alt_e_dep', np.float),
                              ('x_pri', np.float),
@@ -183,58 +203,38 @@ class StraxSimulator(strax.Plugin):
                                ('event_id', np.int),
                                ('channel', np.int), ])
 
-    def load_config(self):
+    def load_resources(self):
         """First load the config through wfsim, then we add some things we'd like to have"""
-        self.resource = load_config(self.config)
+        self.resource = wfsim.load_resource.load_config(self.config)
+        # add mean photon area distribution with avg values.
+        self.resource.mean_photon_area_distribution = epix.Helpers.average_spe_distribution(self.resource.photon_area_distribution)
+        self.resource.fdc_map = wfsim.load_resource.make_map(self.config['field_distortion_correction_map'], fmt='json.gz')
+        # TODO check if NV simulation is working without this stuff
+        #if 'nv_pmt_qe' in self.config['configuration_files'].keys():
+        #            self.resource.nv_pmt_qe = straxen.get_resource(
+        #                self.config['configuration_files']['nv_pmt_qe'], fmt='json')
+        #else:
+        #    warnings.warn('The configuration_files should not exist!'
+        #                  'Everything should come by one config!'
+        #                  'Since nv_pmt_qe config is missing in configuration_files, '
+        #                  'the default one nveto_pmt_qe.json will be used')
+        #    self.resource.nv_pmt_qe = straxen.get_resource('nveto_pmt_qe.json', fmt='json')
 
-        self.resource.s1_map = self.resource.s1_lce_correction_map
-        self.resource.s2_map = self.resource.s2_correction_map
-
-        # Terrible way to handle the config
-        # we should have only one file 
-        # instead of several config
-        if 'nv_pmt_qe' in self.config['configuration_files'].keys():
-                    self.resource.nv_pmt_qe = straxen.get_resource(
-                        self.config['configuration_files']['nv_pmt_qe'], fmt='json')
-        else:
-            warnings.warn('The configuration_files should not exist!'
-                          'Everything should come by one config!'
-                          'Since nv_pmt_qe config is missing in configuration_files, '
-                          'the default one nveto_pmt_qe.json will be used')
-            self.resource.nv_pmt_qe = straxen.get_resource('nveto_pmt_qe.json', fmt='json')
-
-        if 'photon_area_distribution' in self.config['configuration_files'].keys():
-                self.resource.photon_area_distribution = epix.Helpers.average_spe_distribution(
-                                straxen.get_resource(self.config['configuration_files']['photon_area_distribution'], 
-                                fmt='csv'))
-        else:
-            warnings.warn('The configuration_files should not exist!'
-                          'Everything should come by one config!'
-                          'Since photon_area_distribution config is missing in configuration_files, '
-                          'the one in fax_config will be used')
-            self.resource.photon_area_distribution = epix.Helpers.average_spe_distribution(
-                                straxen.get_resource(self.config['photon_area_distribution'], fmt='csv'))
-
-    def setup(self, ):
+    def setup(self):
+        print('Setup')
         overrides = self.config['fax_config_overrides']
         self.config.update(straxen.get_resource(self.config['fax_config'], fmt='json'))
         if overrides is not None:
             self.config.update(overrides)
-
+        # TODO: this should not be needed here. Where is this info normally taken from?
         if 'gains' not in self.config.keys():
-            warnings.warn('Why we have to provide gains here? '
-                          'No gains are passed in fax_config_overrides, default equal to 1')
-            self.config['gains'] =  [1 for i in range(494)]
+            self.config['gains'] =  [1] * 494
         if 'n_top_pmts' not in self.config.keys():
-            warnings.warn('This is a default value, why we have to give it in fax_config_overrides? '
-                          'No n_top_pmts are passed in fax_config_overrides, default equal to 253')
             self.config['n_top_pmts'] =  253
         if 'n_tpc_pmts' not in self.config.keys():
-            warnings.warn('This is a deault value, why we have to give it in fax_config_overrides? '
-                          'No n_tpc_pmts are passed in fax_config_overrides, default equal to 494')
-            self.config['n_tpc_pmts'] =  494  
-
-        self.load_config()
+            self.config['n_tpc_pmts'] =  494
+        print('Loading resources')
+        self.load_resources()
 
     def get_nveto_data(self, ):
         file_loader = epix.io.file_loader(directory=self.config['epix_config']['path'],
@@ -253,16 +253,22 @@ class StraxSimulator(strax.Plugin):
 
     def get_epix_instructions(self, ):
         epix_config = deepcopy(self.config['epix_config'])
-        if epix_config.get('load_from_file', False):
-            return np.load(epix_config['file_name'])
-        detector = epix.init_detector(self.config['detector'].lower(), self.config['detector_config_override'])
-        epix_config['detector_config'] = detector
+        fn = epix_config.get('file_name', '')
+        if fn.endswith('.csv'):
+            print('Loading epix instructions from csv-file')
+            epix_ins = np.load(epix_config['file_name'])
+        elif fn.endswith('.root'):
+            print('Generating epix instructions from root-file')
 
-        outer_cylinder = getattr(epix.detectors, 'xenonnt')
-        _, outer_cylinder = outer_cylinder()
-        epix_config['outer_cylinder'] = outer_cylinder
+            detector = epix.init_detector(self.config['detector'].lower(), self.config['detector_config_override'])
+            epix_config['detector_config'] = detector
 
-        epix_ins = epix.run_epix.main(epix_config, return_wfsim_instructions=True)
+            outer_cylinder = getattr(epix.detectors, 'xenonnt')
+            _, outer_cylinder = outer_cylinder()
+            epix_config['outer_cylinder'] = outer_cylinder
+            epix_ins = epix.run_epix.main(epix_config, return_wfsim_instructions=True)
+
+        # TODO I don't know if I like this.
         save_epix = epix_config.get('save_epix', False)
         if isinstance(save_epix, str):
             # assume save epix as path to store
@@ -278,6 +284,7 @@ class StraxSimulator(strax.Plugin):
         return epix_ins
 
     def compute(self):
+        print('Compute')
         #simulated_data_nveto = self.get_nveto_data()
         simulated_data_nveto = None
         self.epix_instructions = self.get_epix_instructions()
--- epix/simulator/helpers.py
+++ epix/simulator/helpers.py
@@ -1,5 +1,3 @@
-import pickle
-
 import wfsim
 from scipy.interpolate import interp1d
 import numpy as np
@@ -31,32 +29,68 @@ def _merge_these_clusters_nt_res_naive(amp1, r1, z1, amp2, r2, z2, conf):
     SeparationDistance = 1.6  # [cm], the worst case from [[weiss:analysis:he:zresoultion_zdependence]]
     return np.abs(z1 - z2) < SeparationDistance
 
+@numba.njit
+def get_nn_prediction(inp, w0, w1, w2, w3, w4, w5, w6, w7, w8, w9):
+    y      = np.dot(inp, w0) + w1
+    y[y<0] = 0
+    y = np.dot(y, w2) + w3
+    y[y < 0] = 0
+    y = np.dot(y, w4) + w5
+    y[y < 0] = 0
+    y = np.dot(y, w6) + w7
+    y[y < 0] = 0
+    y = np.dot(y, w8) + w9
+    y = 1 / (1+np.exp(-y))
+    return y[0]
 
 def _merge_these_clusters_nt_res_jaron(amp1, r1, z1, amp2, r2, z2, conf):
 
-    lin_corr = {'delta_t': [0.234, 1/1.116], 'width': [0.047, 1/1.176]}
-    v1 = 0.1*conf['field_map']([r1, z1], map_name='drift_speed_map')[0] # cm/us
-    v2 = 0.1*conf['field_map']([r2, z2], map_name='drift_speed_map')[0] # cm/us
-    dt1 = -z1/v1+conf['dt_gate']/1000 # us
-    dt2 = -z2/v2+conf['dt_gate']/1000 # us
-    diff1 = 1e3*conf['diffusion_map']([r1,z1])[0] # cm2/us
-    diff2 = 1e3*conf['diffusion_map']([r2,z2])[0] # cm2/us
-    w1 = lin_corr['width'][1]*1.348*np.sqrt(2*diff1*dt1/v1**2)+lin_corr['width'][0] # us
-    w2 = lin_corr['width'][1]*1.348*np.sqrt(2*diff2*dt2/v2**2)+lin_corr['width'][0] # us
-    delta_t = lin_corr['delta_t'][1]*(dt2-dt1)+lin_corr['delta_t'][0] # us
-    split_param = np.abs(delta_t)/(w1+w2)
-    survival1 = conf['field_map']([r1,z1], map_name='survival_probability_map')[0]
-    survival2 = conf['field_map']([r2,z2], map_name='survival_probability_map')[0]
+    dt_correction = 0.8988671508131871
+    width_correction = 0.854918851953897
+    fm = conf['field_map']
+    dm = conf['diffusion_map']
+    v1 = 0.1*fm([r1, z1], map_name='drift_speed_map')[0] # cm/us
+    #print(f'{v1 =}')
+    v2 = 0.1*fm([r2, z2], map_name='drift_speed_map')[0] # cm/us
+    #print(f'{v2 =}')
+    dt1 = -z1*dt_correction/v1+conf['dt_gate']/1000 # us
+    #print(f'{dt1 =}')
+    dt2 = -z2*dt_correction/v2+conf['dt_gate']/1000 # us
+    #print(f'{dt2 =}')
+    diff1 = 1e3*dm([r1,z1])[0] # cm2/us
+    #print(f'{diff1 =}')
+    diff2 = 1e3*dm([r2,z2])[0] # cm2/us
+    #print(f'{diff2 =}')
+    width1 = width_correction*1.348*np.sqrt(2*diff1*dt1/v1**2) # us
+    #print(f'{w1 =}')
+    width2 = width_correction*1.348*np.sqrt(2*diff2*dt2/v2**2) # us
+    #print(f'{w2 =}')
+    delta_t = (dt2-dt1) # us
+    #print(f'{delta_t =}')
+    split_param = delta_t/(width1+width2)
+    #print(f'{split_param = }')
+    survival1 = fm([r1,z1], map_name='survival_probability_map')[0]
+    survival2 = fm([r2,z2], map_name='survival_probability_map')[0]
     e_lifetime = conf['e_lifetime'] / 1000 # us
-    amp1_corr = conf['e_extraction_yield'] * survival1 * np.exp(-dt1/e_lifetime) * amp1
-    amp2_corr = conf['e_extraction_yield'] * survival2 * np.exp(-dt2/e_lifetime) * amp2
-    return bool(conf['tree'].predict([[split_param, amp1_corr, amp2_corr]]))
+    amp1_corr = int(conf['e_extraction_yield'] * survival1 * np.exp(-dt1/e_lifetime) * amp1)
+    amp2_corr = int(conf['e_extraction_yield'] * survival2 * np.exp(-dt2/e_lifetime) * amp2)
+    #print(f'{amp1_corr = }')
+    #print(f'{amp2_corr = }')
+    lower = min(amp1_corr, amp2_corr)
+    higher = max(amp1_corr, amp2_corr)
+    w0, w1, w2, w3, w4, w5, w6, w7, w8, w9 = conf['nn_weights']
+    X = np.array((split_param, higher, lower), dtype=np.float32)
+    y = get_nn_prediction(X, w0, w1, w2, w3, w4, w5, w6, w7, w8, w9)
+    #print(f'{y = }')
+    return y > np.random.random()
 
-class Helpers():
+
+class Helpers:
     @staticmethod
-    def assignOrder(order):
-        """This is a trick to have the calculation function be executed in a particular order"""
-
+    def assign_order(order):
+        """
+        This is a trick to have the calculation function be executed in a particular order
+        """
         def do_assignment(to_func):
             to_func.order = order
             return to_func
@@ -88,11 +122,12 @@ class Helpers():
         return spe_distribution
 
     @staticmethod
-    def macro_cluster_events(tree, instructions, config):
-        """Loops over all instructions, checks if it's an s2 and if there is another s2 within the same event
+    def macro_cluster_events(nn_weights, instructions, config):
+        """Loops over all instructions, checks if it's an S2 and if there is another s2 within the same event
             within the macro cluster distance, if it is they are merged."""
 
         print(f"\n macro_cluster_events --> s2_clustering_algorithm == {config['s2_clustering_algorithm']} . . .")
+
         merge_config = {}
         if config['s2_clustering_algorithm'] == 'bdt':
             _merge_clusters = _merge_these_clusters_nt_res_jaron
@@ -103,7 +138,7 @@ class Helpers():
                                                      fmt='json.gz', method='WeightedNearestNeighbors')
             merge_config['diffusion_map'] = wfsim.load_resource.make_map(config['diffusion_longitudinal_map'],
                                                       fmt='json.gz', method='WeightedNearestNeighbors')
-            merge_config['tree'] = tree
+            merge_config['nn_weights'] = nn_weights
         elif config['s2_clustering_algorithm'] == 'naive':
             _merge_clusters = _merge_these_clusters_nt_res_naive
         elif config['s2_clustering_algorithm'] == 'nsort':
@@ -113,33 +148,41 @@ class Helpers():
         for ix1, _ in enumerate(instructions):
             if instructions[ix1]['type'] != 2:
                 continue
+            #print(f"{ix1} is an S2 of event {instructions[ix1]['event_number']}. Other peaks in this event:")
+            #print(instructions[instructions['event_number'] == instructions[ix1]['event_number']])
             for ix2 in range(1, len(instructions[ix1:])): # why was  + 1): ?
-                if instructions[ix1 + ix2]['type'] != 2:
-                    continue
+                #print(ix1 + ix2, end=' ')
                 if instructions[ix1]['event_number'] != instructions[ix1 + ix2]['event_number']:
+                    #print('belongs to another event.')
                     break
-
-                # _nt_res
+                if instructions[ix1 + ix2]['type'] != 2:
+                    #print('isn\'t an S2')
+                    continue
+                #print(f'is an S2 of the same event. Check merge...')
                 r1 = np.sqrt(instructions[ix1]['x'] ** 2 + instructions[ix1]['y'] ** 2)
                 r2 = np.sqrt(instructions[ix1 + ix2]['x'] ** 2 + instructions[ix1 + ix2]['y'] ** 2)
                 if _merge_clusters(instructions[ix1]['amp'], r1, instructions[ix1]['z'],
                                    instructions[ix1 + ix2]['amp'], r2, instructions[ix1 + ix2]['z'],
                                    merge_config):
+                    #print(f'I will merge {ix1} and {ix1+ix2}.')
+                    amp1 = instructions[ix1]['amp']
+                    amp2 = instructions[ix1 + ix2]['amp']
+                    amp_total = int((instructions[ix1]['amp'] + instructions[ix1 + ix2]['amp']))
+                    instructions[ix1 + ix2]['x'] = (instructions[ix1]['x'] * amp1 + instructions[ix1 + ix2]['x'] * amp2) / amp_total
+                    instructions[ix1 + ix2]['y'] = (instructions[ix1]['y'] * amp1+ instructions[ix1 + ix2]['y'] * amp2) / amp_total
+                    instructions[ix1 + ix2]['z'] = (instructions[ix1]['z'] + instructions[ix1 + ix2]['z']) / 2
 
-                    instructions[ix1 + ix2]['x'] = (instructions[ix1]['x'] + instructions[ix1 + ix2]['x']) * 0.5
-                    instructions[ix1 + ix2]['y'] = (instructions[ix1]['y'] + instructions[ix1 + ix2]['y']) * 0.5
-                    instructions[ix1 + ix2]['z'] = (instructions[ix1]['z'] + instructions[ix1 + ix2]['z']) * 0.5
-
-                    # prymary position is one
+                    # primary position is one
                     instructions[ix1 + ix2]['x_pri'] = instructions[ix1]['x_pri']
                     instructions[ix1 + ix2]['y_pri'] = instructions[ix1]['y_pri']
                     instructions[ix1 + ix2]['z_pri'] = instructions[ix1]['z_pri']
 
-                    instructions[ix1 + ix2]['amp'] = int((instructions[ix1]['amp'] + instructions[ix1 + ix2]['amp']))
+                    instructions[ix1 + ix2]['amp'] = amp_total
                     instructions[ix1]['amp'] = -1  # flag to throw this instruction away later
                     instructions[ix1 + ix2]['e_dep'] = (instructions[ix1]['e_dep'] + instructions[ix1 + ix2]['e_dep'])
                     instructions[ix1]['e_dep'] = -1  # flag to throw this instruction away later
                     break
+                #print(f'I won\'t merge {ix1} and {ix1+ix2}.')
 
     @staticmethod
     def get_s1_area_with_spe(spe_distribution, num_photons):
@@ -170,10 +213,16 @@ class Helpers():
                                            resource=resource)
 
     @staticmethod
-    def get_s2_charge_yield(n_electron, positions, z_obs, config, resource):
+    def get_s2_charge_yield(n_electron, xy, z, config, resource):
         """See wfsim.s2.get_electron_yield"""
         return wfsim.S2.get_electron_yield(n_electron=n_electron,
-                                           xy_int=positions,
-                                           z_int=z_obs,
+                                           xy_int=xy,
+                                           z_int=z,
                                            config=config,
                                            resource=resource)
+
+    @staticmethod
+    def get_drift_time(z, xy, config, resource):
+        """See wfsim.S2.get_s2_drift_time_params"""
+        dt_mean, _ = wfsim.S2.get_s2_drift_time_params(z, xy, config, resource)
+        return dt_mean
